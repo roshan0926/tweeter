4598873535cd8eb4e990ef6dd060346f27a025a5
Roshan Nasseri <roshan.sep26@gmail.com>
2021-04-20 19:00:15 +0000

    add personalized styles (make tweeter dark themed) & implemet counter function on text input

--- public/scripts/composer-char-counter.js
+++ public/scripts/composer-char-counter.js
@@ -1,3 +1,16 @@
 $(document).ready(function() {
-  // --- our code goes here ---
-});
+  $('textarea').on('input', function (event) {
+    let length = $(this).val().length
+    let $counter= $(this).parent('form').find('.counter')
+    updateCountdown($counter, length)
+  })
+})
+
+function updateCountdown($counter, length) {
+  let charsLeft = 140 - length
+  $counter.text(charsLeft)
+  $counter.css('color', 'black')
+  if (charsLeft < 0) {
+    $counter.css('color', 'red')
+  }
+}
